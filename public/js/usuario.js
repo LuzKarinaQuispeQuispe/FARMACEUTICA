3d73539ffe9b42f1e8549d4ae44bde6f3a73b671
Oscar Flores <93671002+OscarAFT@users.noreply.github.com>
2024-12-01 18:04:04 -0500

    Agregacion de eventos, validaciones, busqueda

--- public/js/usuario.js
+++ public/js/usuario.js
@@ -1,5 +1,6 @@
-let filtroDataAdded = false; 
-let productosData; 
+let filtroDataAdded = false; // Bandera para indicar si se ha agregado el evento de filtrado
+let productosData; // Variable global para almacenar los datos
+// Carga los eventos principales
 document.addEventListener('DOMContentLoaded', function() {
     setupEventListeners();
     initializePage();
@@ -356,3 +357,247 @@ function crearFilaProducto(producto) {
 
 }
 
+//agrega las funciones relacionadas con el filtrado
+function agregarEventosFiltrado(data) {
+    document.querySelectorAll('.thfiltro').forEach(th => {
+        th.addEventListener('click', function(event) {
+            event.preventDefault();
+            ordenarProductos(th, data);
+        });
+    });
+}
+// ordena los productos segun el filtro
+function ordenarProductos(th, data) {
+    console.log("primer llamado");
+
+    console.log(th.dataset.order);
+    const field = th.getAttribute('data-field');
+    const sortOrder = th.getAttribute('data-order');
+    const newOrder = sortOrder === 'asc' ? 'desc' : 'asc';
+    th.setAttribute('data-order', newOrder);
+    console.log(th.dataset.order);
+    data.sort((a, b) => {
+        if (field === 'nombre') return newOrder === 'asc' ? a.Nombre.localeCompare(b.Nombre) : b.Nombre.localeCompare(a.Nombre);
+        if (field === 'precio') return newOrder === 'asc' ? a.Usua.localeCompare(b.Usua) : b.Usua.localeCompare(a.Usua);
+        if (field === 'categoria') return newOrder === 'asc' ? a.TipoUsuario_id.localeCompare(b.TipoUsuario_id) : b.TipoUsuario_id.localeCompare(a.TipoUsuario_id);
+
+    });
+
+    actualizarTablaProductos(data, parseInt(document.getElementById('currentPage').textContent), parseInt(document.getElementById('itemsPerPage').value),1);
+}
+//agrega los eventos de edicion y eliminacion a los registros
+function agregarEventosEdicionYEliminacion() {
+    document.querySelectorAll('.eliminar-producto').forEach(button => {
+        button.addEventListener('click', function(event) {
+            event.preventDefault();
+            if (confirm('¿Estás seguro de que deseas eliminar este usuario?')) {
+                eliminarProducto(this.getAttribute('data-id'));
+            }
+
+        });
+    });
+
+    document.querySelectorAll('.editarproducto').forEach(button => {
+        button.addEventListener('click', function(event) {
+            event.preventDefault();
+            mostrarModalEdicion(
+                this.getAttribute('data-id'),
+                this.getAttribute('nombre'),
+                this.getAttribute('usuario'),
+                this.getAttribute('tipousuario')
+            );
+        });
+    });
+    
+    document.querySelector('.modal-content .close').addEventListener('click', cerrarModalEdicion);
+    document.getElementById('formEditarProducto').addEventListener('submit', handleEditarProductoSubmit);
+}
+//muestra modal de edicion
+function mostrarModalEdicion(idProducto,nombre,usuario,tipousuario) {
+    toggleSidebar();
+    const modal = document.getElementById('myModal');
+    modal.setAttribute('data-id', idProducto);
+    modal.style.display = 'block';
+    document.getElementById('autoSizingInputnombreEditar').value=nombre;
+    document.getElementById('autoSizingInputprecioEditar').value=usuario;
+    document.getElementById('autoSizingSelectcatEditar').value=tipousuario;
+    
+
+
+
+
+
+
+}
+//cierra el modal de edicion
+function cerrarModalEdicion() {
+    restaurarThemeEdit();
+    document.getElementById('myModal').style.display = 'none';
+    toggleSidebar();
+}
+//restaura los estilos anteriores del manejo de errores para formulario editar
+function handleEditarProductoSubmit(event) {
+    event.preventDefault();
+    const idProducto = document.getElementById('myModal').getAttribute('data-id');
+
+    const productoEditado = {
+        nombre: document.getElementById('autoSizingInputnombreEditar').value,
+        usuario: document.getElementById('autoSizingInputprecioEditar').value,
+        contraseña: document.getElementById('autoSizingInputPasswordEditar').value,
+        contraseña2: document.getElementById('autoSizingInputConfirmPasswordEditar').value,
+        categoria: document.getElementById('autoSizingSelectcatEditar').value
+    };
+
+    const errors = []; // Array para almacenar los errores
+
+    const regex = /^[a-zA-Z\s]*$/;
+
+    const nombreInput = document.getElementById('autoSizingInputnombreEditar');
+    let nombre = nombreInput.value.trim();
+
+    if (nombre === '') {
+        errors.push('El campo de nombre no puede estar vacío.');
+        nombreInput.style.borderColor = 'red';
+    } else if (!regex.test(nombre)) {
+        errors.push('El nombre debe contener solo letras y espacios.');
+        nombreInput.style.borderColor = 'red';
+    } else if (nombre.length > 30) {
+        errors.push('El nombre no puede exceder los 30 caracteres.');
+        nombreInput.style.borderColor = 'red';
+    } else {
+        nombreInput.style.borderColor = ''; // Restablecer el borde a su color normal
+    }
+
+    const usuarioInput = document.getElementById('autoSizingInputprecioEditar');
+    let usuario = usuarioInput.value.trim();
+
+    if (usuario === '') {
+        errors.push('El campo de usuario no puede estar vacío.');
+        usuarioInput.style.borderColor = 'red';
+    } else if (!regex.test(usuario)) {
+        errors.push('El usuario debe contener solo letras y espacios.');
+        usuarioInput.style.borderColor = 'red';
+    } else if (usuario.length > 30) {
+        errors.push('El usuario no puede exceder los 30 caracteres.');
+        usuarioInput.style.borderColor = 'red';
+    } else {
+        usuarioInput.style.borderColor = ''; // Restablecer el borde a su color normal
+    }
+
+    const contraInput = document.getElementById('autoSizingInputPasswordEditar');
+    let contraseña = contraInput.value;
+
+    if (contraseña === '') {
+        errors.push('El campo de contraseña no puede estar vacío.');
+        contraInput.style.borderColor = 'red';
+    } else {
+        contraInput.style.borderColor = ''; // Restablecer el borde a su color normal
+    }
+
+    const contra2Input = document.getElementById('autoSizingInputConfirmPasswordEditar');
+    let contraseña2 = contra2Input.value;
+
+    if (contraseña2 === '') {
+        errors.push('Debes confirmar la contraseña.');
+        contra2Input.style.borderColor = 'red';
+    } else if (contraseña !== contraseña2) {
+        errors.push('Las contraseñas no coinciden.');
+        contraInput.style.borderColor = 'red';
+        contra2Input.style.borderColor = 'red';
+    } else {
+        contraInput.style.borderColor = ''; // Restablecer el borde a su color normal
+        contra2Input.style.borderColor = ''; // Restablecer el borde a su color normal
+    }
+
+    const categoriaInput = document.getElementById('autoSizingSelectcatEditar');
+    let categoria = categoriaInput.value;
+    if (categoria === '') {
+        errors.push('Debes seleccionar una categoría.');
+        categoriaInput.style.borderColor = 'red';
+    } else {
+        categoriaInput.style.borderColor = ''; // Restablecer el borde a su color normal
+    }
+
+    // Mostrar todos los errores juntos si existen
+    if (errors.length > 0) {
+        alert(errors.join('\n'));
+        return null; // Devolver null si hay errores
+    }
+
+    if (confirm('¿Estás seguro de que deseas editar este usuario?')) {
+        editarProducto(idProducto, productoEditado);
+        cerrarModalEdicion();
+    }
+
+
+   
+    
+}
+// oculta el sidebar
+function toggleSidebar() {
+    if (window.innerWidth > 991) {
+        document.body.classList.toggle('sb-sidenav-toggled');
+        localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+    }
+}
+
+document.getElementById("btnAgregar").addEventListener("click", function() {
+    var formulario = document.getElementById("formularioProducto");
+    
+    // Alterna la visibilidad del formulario
+    if (formulario.style.display === "none" || formulario.style.display === "") {
+        formulario.style.display = "block";  // Muestra el formulario
+    } else {
+        formulario.style.display = "none";  // Oculta el formulario
+    }
+});
+
+function eliminarProducto(idProducto) {
+    const searchTerm = document.querySelector('.form-control');
+    const originalSearchTerm = searchTerm.value.trim().toLowerCase();
+    console.log(idProducto);
+    fetch(`http://localhost/Farmacia/src/controllers/ManejoUsuario.php?id=${idProducto}`, {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json' }
+    })
+    .then(response => {
+        if (response.ok) {
+            console.log('Usuario eliminado correctamente.');
+            actualizarTabla();
+            searchTerm.value = '';
+        } else {
+            console.error('Error al eliminar el usuario.');
+        }
+    })
+    .catch(error => console.error('Error en la solicitud:', error));
+}
+
+function editarProducto(idProducto, datosProducto) {
+    const searchTerm = document.querySelector('.form-control');
+    const originalSearchTerm = searchTerm.value.trim().toLowerCase();
+
+    fetch(`/controllers/ManejoUsuario.php?id=${idProducto}`, {
+        method: 'EDIT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(datosProducto)
+    })
+    .then(response => {
+        if (response.ok) {
+            console.log(JSON.stringify(datosProducto));
+            console.log('Usuario editado correctamente.');
+            actualizarTabla();
+            searchTerm.value = '';
+
+            //aqui quiero realizar la busqueda del elemento que se encontraba en el buscador
+        } else {
+            console.error('Error al editar el Usuario.');
+        }
+    })
+    .catch(error => console.error('Error en la solicitud:', error));
+}
+
+function actualizarTabla() {
+    const itemsPerPage = parseInt(document.getElementById('itemsPerPage').value);
+    const currentPage = parseInt(document.getElementById('currentPage').textContent);
+    obtenerProductos(currentPage, itemsPerPage);
+}
