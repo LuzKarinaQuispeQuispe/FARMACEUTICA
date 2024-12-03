2f026bb3dc0444e82fa5fe685e609f3e0bb9dd74
Oscar Flores <93671002+OscarAFT@users.noreply.github.com>
2024-12-02 22:49:03 -0500

    actualización_conection.php

--- config/conection.php
+++ config/conection.php
@@ -1,17 +1,15 @@
 <?php
 
 class Database {
-    private $host = "localhost";
-    private $dsn = "mysql:host=localhost;dbname=farmacia_esis";
-    private $db_name = "farmacia_esis";
-    private $username = "root";
-    private $password = "73008057";
+    private $host = "161.35.224.168";
+    private $dsn = "mysql:host=161.35.224.168;dbname=esis-farmacia";
+    private $db_name = "esis-farmacia";
+    private $username = "dev1";
+    private $password = "ESIS_2024_dev1";
     public $conn;
-
     public function getConnection() {
         $this->conn = null;
         try {
-           
             $this->conn = new PDO($this->dsn, $this->username, $this->password);
             $this->conn->exec("set names utf8");
         } catch(PDOException $exception) {
